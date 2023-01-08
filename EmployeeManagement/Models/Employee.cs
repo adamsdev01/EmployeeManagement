de71c9e473e92691161419624bc9e788d90dc962
Tiffany Adams <tadams03@mail.roosevelt.edu>
2023-01-08 16:32:28 -0600

    Create new Blazor Server Project

---
+++ EmployeeManagement/Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿namespace Models
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public DateTime DOB { get; set; }
+        public Gender Gender { get; set; }
+        public Department Department { get; set; }
+        public string PhotoPath { get; set; }
+
+    }
+}
